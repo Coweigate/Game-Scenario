abf1ebc11a8de8f96c6520be7ffe38879ccdb790
stefkay <skaykieva@gmail.com>
2015-09-29 16:23:41 +0300

    initial commit

---
+++ FindTheLettersGame/FindTheLettersGame/FindTheLettersGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheLettersGame
+{
+    class FindTheLettersGame
+    {
+        /*static variables*/
+
+
+
+
+
+        /*---------------------*/
+
+        /*main method*/
+        static void Main(string[] args)
+        {
+            /*intro part*/
+            
+            /*end of intro part*/
+
+
+
+        }
+        /*----------------------*/
+        /*methods for intro part*/
+
+
+
+
+
+
+        /*----------------------*/
+        /*matrix generator*/
+
+
+
+
+
+
+        /*----------------------*/
+
+    }
+}
