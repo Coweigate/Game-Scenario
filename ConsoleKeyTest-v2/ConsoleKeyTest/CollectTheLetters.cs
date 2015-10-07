04de1d08445fcfb4e4b48a85cd42b39213168258
ggsomnoev <georgisomnoev@yahoo.com>
2015-10-07 03:34:30 +0300

    The letters now move, they have random color. The user can see the order in which they have to collect them and the remaining number of letters. There is a bug, if the letter spawns near a wall it moves through it back and forth. I will add a timer.
    Tomorrow we will merge the work and try to fix the bugs.

---
+++ ConsoleKeyTest-v2/ConsoleKeyTest/CollectTheLetters.cs
@@ -0,0 +1,147 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace CollectTheLettersTestVersion
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            Console.WriteLine("To stop the game press ESC.\t\tFor movement use 'W', 'S', 'A' and 'D'");
+
+            Console.CursorVisible = false;
+            int levelChoice = 1;//we can use random level option in the menu and a specific level option
+
+            //creating initial matrix
+            Matrix matrix = new Matrix(levelChoice);
+            //drawing the matrix to the console
+            matrix.DrawMatrix();
+
+            //creating a player
+            Player player = new Player();
+            //drawing the player to the console
+            player.DrawPlayer();
+            player.PlayerScore();
+
+            //get 10 random letters
+            Random randomNum = new Random();
+            int letterToCollect = 10;
+            List<Letters> lettersToWrite = new List<Letters>();
+            for (int i = 0; i < letterToCollect; i++)
+            {
+                lettersToWrite.Add(new Letters(matrix, randomNum));
+            }
+            //drawing the player to the console inside the matrix & check if it didn't spawn on top of the player
+            while (player.X == lettersToWrite[0].X && player.Y == lettersToWrite[0].Y)
+            {
+                lettersToWrite[0].GetRandomPosition();
+            }
+            lettersToWrite[0].DrawLetter();
+            //avoid letters spawning on top of each other or on top of the player
+            for (int i = 0; i < lettersToWrite.Count - 1; i++)
+            {
+                while ((lettersToWrite[i].X == lettersToWrite[i + 1].X
+                    && lettersToWrite[i].Y == lettersToWrite[i + 1].Y)
+                    || (player.X == lettersToWrite[i + 1].X && player.Y == lettersToWrite[i + 1].Y))
+                {
+                    lettersToWrite[i + 1].GetRandomPosition();
+                }
+                lettersToWrite[i + 1].DrawLetter();
+            }
+            //draw the remaining lettes to collect
+            Letters.RemainingLetters(lettersToWrite);
+
+            //getting user input(pressed key)
+            ConsoleKey pressedKey = Console.ReadKey(true).Key;
+            int letterOrder;
+            while (pressedKey != ConsoleKey.Escape)
+            {
+                //looping only if key is pressed
+                while (Console.KeyAvailable && lettersToWrite.Count > 0)
+                {
+                    pressedKey = Console.ReadKey(true).Key;
+                    //updating player location based on the pressedKey, also printing him to the console.                        
+                    player.ChangePlayerLocation(pressedKey, matrix);
+
+                    letterOrder = 0;
+                    if (lettersToWrite.Count > 0)
+                    {
+                        //check if the player steped on a letter                        
+                        for (int i = 0; i < lettersToWrite.Count; i++)
+                        {
+                            if (player.X == (lettersToWrite[i].X) && player.Y == (lettersToWrite[i].Y) && lettersToWrite[i] == lettersToWrite[letterOrder])
+                            {
+                                player.GetPoint();
+                                player.PlayerScore();
+                                lettersToWrite.RemoveAt(i);
+                                letterOrder++;
+                                //update the remaining lettes
+                                Letters.RemainingLetters(lettersToWrite);
+                                break;
+                            }
+                            else if (player.X == (lettersToWrite[i].X) && player.Y == (lettersToWrite[i].Y))
+                            {
+                                player.LosePoint();
+                                player.PlayerScore();
+                                player.DrawPlayer();
+                                lettersToWrite[i].hasBeenStepedOver = true;
+                            }
+                            else if (lettersToWrite[i].hasBeenStepedOver)
+                            {
+                                lettersToWrite[i].DrawLetter();
+                                lettersToWrite[i].hasBeenStepedOver = false;
+                                player.DrawPlayer();
+                            }
+                        }
+                    }                    
+                }
+                letterOrder = 0;
+                if (lettersToWrite.Count > 0)
+                {
+                    //check if the player steped on a letter                        
+                    for (int i = 0; i < lettersToWrite.Count; i++)
+                    {
+                        if (player.X == (lettersToWrite[i].X) && player.Y == (lettersToWrite[i].Y) && lettersToWrite[i] == lettersToWrite[letterOrder])
+                        {
+                            player.GetPoint();
+                            player.PlayerScore();
+                            player.DrawPlayer();
+                            lettersToWrite.RemoveAt(i);
+                            letterOrder++;
+                            //update the remaining lettes
+                            Letters.RemainingLetters(lettersToWrite);
+                            break;
+                        }
+                        else if (player.X == (lettersToWrite[i].X) && player.Y == (lettersToWrite[i].Y))
+                        {
+                            player.LosePoint();
+                            player.PlayerScore();
+                            player.DrawPlayer();
+                            lettersToWrite[i].hasBeenStepedOver = true;
+                        }
+                        else if (lettersToWrite[i].hasBeenStepedOver)
+                        {
+                            lettersToWrite[i].DrawLetter();
+                            lettersToWrite[i].hasBeenStepedOver = false;
+                            player.DrawPlayer();
+                        }
+                    }
+                }
+                //update the letters;
+                for (int i = 0; i < lettersToWrite.Count; i++)
+                {
+                    lettersToWrite[i].Update();
+                }
+            }
+            //press any key to continue message
+            Console.SetCursorPosition(0, Console.WindowHeight - 2);
+            Console.WriteLine();
+            player = null;
+            matrix = null;
+            Console.Clear();
+            Main();
+            
+    }
+    }
+}
---
+++ ConsoleKeyTest-v2/ConsoleKeyTest/Letters.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace CollectTheLettersTestVersion
+{
+    class Letters
+    {
+        public char letter;        
+        Matrix matrixBoard = new Matrix(1);
+        public ConsoleColor letterColor;
+        public bool hasBeenStepedOver = false;        
+        char[] letters = new char[26];
+        int leftBorder, rightBorder, topBorder, bottomBorder, x, y, randomX, randomY;
+        Random randomGenerator;
+        Stopwatch timer = new Stopwatch();
+        int timeToStartMoving;
+
+        public int X
+        {
+            get { return x; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+        }
+
+        public Letters(Matrix matrix, Random random) {
+            //filling the matrix with letters from [65]A to[90]Z
+            int num = 65;
+            for (int i = 0; i < letters.Length; i++)
+            {
+                letters[i] = (char)num;
+                num++;
+            }
+            randomGenerator = random;
+            //setting the letter color
+            letterColor = LetterColor(randomGenerator.Next(1, 9));
+            //setting the letter moving time
+            timeToStartMoving = randomGenerator.Next(150, 251);
+            //getting the matrix borders
+            leftBorder = matrix.leftBorder + 1;
+            rightBorder = matrix.rightBorder;
+            topBorder = matrix.topBorder + 1;
+            bottomBorder = matrix.bottomBorder;
+
+            GetRandomLetter();
+            GetRandomPosition();
+            randomX = GetRandomDirection();
+            randomY = GetRandomDirection();
+        }
+
+        //get a random letter
+        public void GetRandomLetter() {
+            letter = letters[randomGenerator.Next(0, letters.Length)];            
+        }
+        public void GetRandomPosition() {
+            x = randomGenerator.Next(leftBorder, rightBorder);
+            y = randomGenerator.Next(topBorder, bottomBorder);
+            //to check for collision with the matrix board
+            //while (matrixBoard.matrix[randomXPosition, randomYPosition] != " ")
+            //{
+            //    randomXPosition = randomGenerator.Next(leftBorder, rightBorder);
+            //    randomYPosition = randomGenerator.Next(topBorder, bottomBorder);
+            //}
+        }
+        //draws a letter inside the matrix
+        public void DrawLetter() {    
+            //setting the cursor at random position and drawing a letter
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = letterColor;
+            Console.Write(letter);
+            Console.ResetColor();
+        }
+        public void Update() {
+            //moving part
+            timer.Start();
+            if (timer.ElapsedMilliseconds == timeToStartMoving)
+            {
+                MoveLetter();
+                DrawLetter();
+                timer.Reset();
+            }
+        }
+
+        private ConsoleColor LetterColor(int num)
+        {
+            switch (num) {
+                case 1:
+                    return ConsoleColor.Blue;
+                case 2:
+                    return ConsoleColor.Yellow;
+                case 3:
+                    return ConsoleColor.Green;
+                case 4:
+                    return ConsoleColor.Cyan;
+                case 5:
+                    return ConsoleColor.Magenta;
+                case 6:
+                    return ConsoleColor.DarkMagenta;
+                case 7:
+                    return ConsoleColor.DarkYellow;
+                case 8:
+                    return ConsoleColor.DarkGreen;
+                default: return ConsoleColor.White;
+            }
+        }
+        
+        public void MoveLetter() {    
+            Console.SetCursorPosition(x, y);
+            Console.Write(" ");
+            x += randomX;
+            y += randomY;
+            if (x <= (matrixBoard.leftBorder + 1) || x >= (matrixBoard.rightBorder  - 1)) {
+                randomX *= -1;
+            }
+            if (y <= (matrixBoard.topBorder + 1) || y >= (matrixBoard.bottomBorder - 1)) {
+
+                randomY *= -1;
+            }
+        }
+
+        private int GetRandomDirection() {
+            int[] direction = { -1, 1 };
+            return direction[randomGenerator.Next(0, 2)];
+        }
+
+        public static void RemainingLetters(List<Letters> listOfLetters)
+        {
+            string remainingLetters = String.Format("Remaining letters: {0}", listOfLetters.Count);
+            //to clear the screen on each update
+            Console.SetCursorPosition(0, Console.WindowHeight - 3);
+            Console.Write(new string(' ', remainingLetters.Length + 1));
+
+            Console.SetCursorPosition(0, Console.WindowHeight - 3);
+            Console.Write("Remaining letters: {0}", listOfLetters.Count);
+            DrawRemainingLettersOrder(listOfLetters, (remainingLetters.Length + 1));
+        }
+
+        private static void DrawRemainingLettersOrder(List<Letters> listOfLetters, int position)
+        {
+            //clearing the screen
+            Console.SetCursorPosition(position, Console.WindowHeight - 3);
+            Console.Write(new string(' ', listOfLetters.Count + 2));
+
+            for (int i = 0; i < listOfLetters.Count; i++)
+            {
+                Console.SetCursorPosition((position + i), Console.WindowHeight - 3);
+                Console.ForegroundColor = listOfLetters[i].letterColor;
+                Console.Write(listOfLetters[i].letter);
+            }
+            Console.ResetColor();
+        }
+    }
+}
